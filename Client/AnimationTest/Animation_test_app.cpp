28f44c7941076554bb85042c2bf52bfb065bf71a
gws1017 <gws1017@naver.com>
2022-01-25 18:28:32 +0900

    [Dev] 프로젝트 생성 및 준비
    - 애니메이션 코드를 적용하기전 기본적인 코드 추가함

---
+++ Client/AnimationTest/Animation_test_app.cpp
@@ -0,0 +1,38 @@
+#include <include/client_fw.h>
+#include <client/core/entry_point.h>
+#include "object/level/player_test_level.h"
+using namespace client_fw;
+
+namespace animation_test
+{
+	class AnimationTestApp : public client_fw::Application
+	{
+	public:
+		AnimationTestApp() : Application(L"Animation Test App")
+		{
+
+		}
+		bool Initialize() override
+		{
+			bool result = Application::Initialize();
+			RegisterPressedEvent("Clip Cursor", std::vector{ EventKeyInfo{eKey::kF3, {eAdditionalKey::kControl}} },
+				[]()->bool {Input::SetClipCursor(!Input::IsClipCursor()); return true;  });
+			RegisterPressedEvent("Hide Cursor", std::vector{ EventKeyInfo{eKey::kF2, {eAdditionalKey::kControl}} },
+				[]()->bool {Input::SetHideCursor(!Input::IsHideCursor()); return true;  });
+
+			RegisterPressedEvent("open player test level", { {eKey::k2} },
+				[this]()->bool {OpenLevel(CreateSPtr<PlayerTestLevel>()); return true; });
+			return result;
+		}
+		void Shutdown() override
+		{
+			Application::Shutdown();
+		}
+	};
+}
+
+
+client_fw::UPtr<client_fw::Application> client_fw::CreateApplication()
+{
+	return client_fw::CreateUPtr<animation_test::AnimationTestApp>();
+}
