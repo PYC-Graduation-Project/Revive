05c39e2676c87e9ae5dc7722c65e9973128e948c
gws1017 <gws1017@naver.com>
2022-01-28 19:14:45 +0900

    [Dev] MeshLoader #1
    - fbx를 익스포팅한 rev확장자 파일을 읽어들이는 함수 LoadLev를 작성함, 기본 구조는 LoadObj와 유사함
    
    - LoadObj에서 ifstream 사용하기에 바이너리 파일 읽을때도 이용 기본적으로 ifstream의 read함수를 이용한다
      바이너리 파일은 "문자열  길이 / 문자열" 형태로 저장되기 때문에 문자열 길이를 먼저 읽어들이고 그 길이만큼
      문자열을 읽는 ReadFromFile() 함수를 작성함. 이전에 사용했던 fread를 이용하는 형태와 거의 같음, 이 함수는 파일을
      읽는 데 기본이 될 것 이다.
    
    - <HierArchy> 단어를 읽어 보는 것 까지 테스트 해보았음. 아직 정상적으로 파일 전부를 읽는 것이 아니다.

--- Client/ClientFW/src/client/asset/mesh/mesh_loader.cpp
+++ Client/ClientFW/src/client/asset/mesh/mesh_loader.cpp
@@ -14,6 +14,9 @@ namespace client_fw
 		case HashCode(".obj"):
 			mesh = LoadObj(name, path, extension);
 			break;
+		case HashCode(".rev"):
+			mesh = LoadRev(name, path, extension);
+			break;
 		default:
 			LOG_ERROR("Files in {0} format cannot be supported", extension);
 			break;
@@ -188,5 +191,242 @@ namespace client_fw
 		mesh->SetOrientBox(oriented_box);
 
 		return mesh;
+	}
+
+	SPtr<StaticMesh> MeshLoader::LoadRev(const std::string& name, const std::string& path, const std::string& extension)
+	{
+		std::ifstream rev_file(path);
+
+		if (rev_file.is_open() == false)
+		{
+			LOG_ERROR("Could not find path : [{0}]", path);
+			return nullptr;
+		}
+
+		std::vector<Vec3> positions;
+		std::vector<Vec3> normals;
+		std::vector<Vec2> tex_coords;
+		std::vector<CombineData> combine_data;
+		UINT combine_data_index = 0;
+
+		std::stringstream ss;
+		std::string line;
+		std::string prefix;
+
+		Vec3 temp_vec;
+		UINT temp_uint = 0;
+		std::string temp_string;
+
+		Vec3 max_pos{ -FLT_MAX, -FLT_MAX, -FLT_MAX };
+		Vec3 min_pos{ FLT_MAX, FLT_MAX, FLT_MAX };
+
+		auto UpdateMaxMin([&max_pos, &min_pos](const Vec3& pos) {
+			max_pos.x = max(max_pos.x, pos.x);
+			max_pos.y = max(max_pos.y, pos.y);
+			max_pos.z = max(max_pos.z, pos.z);
+
+			min_pos.x = min(min_pos.x, pos.x);
+			min_pos.y = min(min_pos.y, pos.y);
+			min_pos.z = min(min_pos.z, pos.z);
+			});
+
+		while (ReadFromFile(&rev_file,&ss))
+		{
+			ss >> prefix;
+
+			switch (HashCode(prefix.c_str()))
+			{
+			case HashCode("<Hierarchy>"):
+				
+				LoadFrameHierArchy(&rev_file);
+				ss >> temp_vec.x >> temp_vec.y >> temp_vec.z;
+				temp_vec.z *= -1.0f;
+				UpdateMaxMin(temp_vec);
+				positions.emplace_back(std::move(temp_vec));
+				break;
+			case HashCode("<Animation>"):
+				ss >> temp_vec.x >> temp_vec.y >> temp_vec.z;
+				temp_vec.y = 1.0f - temp_vec.y;
+				tex_coords.emplace_back(Vec2{ temp_vec.x, temp_vec.y });
+				break;
+			case HashCode("</Animation>"):
+				ss >> temp_vec.x >> temp_vec.y >> temp_vec.z;
+				temp_vec.z *= -1.0f;
+				normals.emplace_back(std::move(temp_vec));
+				break;
+			case HashCode("usemtl"):
+			{
+				ss >> temp_string;
+				auto iter = std::find_if(combine_data.cbegin(), combine_data.cend(),
+					[temp_string](const CombineData& data) {
+						return data.name == temp_string;
+					});
+				if (iter == combine_data.cend())
+				{
+					combine_data.push_back({ temp_string });
+					combine_data_index = static_cast<UINT>(combine_data.size()) - 1;
+				}
+				else
+				{
+					combine_data_index = static_cast<UINT>(std::distance(combine_data.cbegin(), iter));
+				}
+			}
+			break;
+			case HashCode("f"):
+			{
+				int count = 0;
+
+				while (ss >> temp_uint)
+				{
+					if (count == 0)
+						combine_data[combine_data_index].pos_indices.emplace_back(temp_uint - 1);
+					else if (count == 1)
+						combine_data[combine_data_index].tex_indices.emplace_back(temp_uint - 1);
+					else if (count == 2)
+						combine_data[combine_data_index].normal_indices.emplace_back(temp_uint - 1);
+
+					if (ss.peek() == '/')
+					{
+						++count;
+						ss.ignore();
+					}
+					else if (ss.peek() == ' ')
+					{
+						ss.ignore(1, ' ');
+						count = 0;
+					}
+				}
+			}
+			break;
+			default:
+				break;
+			}
+		}
+
+		SPtr<StaticMesh> mesh = CreateSPtr<StaticMesh>();
+
+		UINT vertex_count = 0;
+		for (const auto& data : combine_data)
+			vertex_count += static_cast<UINT>(data.pos_indices.size());
+
+		std::vector<TextureLightVertex> vertices(vertex_count);
+
+		vertex_count = 0;
+		for (const auto& data : combine_data)
+		{
+			UINT count = static_cast<UINT>(data.pos_indices.size());
+			mesh->AddInstanceInfo({ count, vertex_count });
+
+			for (UINT i = 0; i < count; ++i)
+			{
+				UINT index = i + vertex_count;
+				if (index % 3 == 0)
+					index += 2;
+				else if (index % 3 == 2)
+					index -= 2;
+
+				vertices[index].SetPosition(positions[data.pos_indices[i]]);
+				vertices[index].SetTexCoord(tex_coords[data.tex_indices[i]]);
+				vertices[index].SetNormal(normals[data.normal_indices[i]]);
+			}
+
+			vertex_count += count;
+		}
+
+		const UINT vertices_size = vertex_count * sizeof(TextureLightVertex);
+		if (FAILED(D3DCreateBlob(vertices_size, &mesh->GetVertexBufferBlob())))
+		{
+			LOG_ERROR("Could not create blob for vertex");
+			return nullptr;
+		}
+		CopyMemory(mesh->GetVertexBufferBlob()->GetBufferPointer(), vertices.data(), vertices_size);
+
+		Vec3 center = max_pos + min_pos;
+		Vec3 extents = (max_pos - min_pos) * 0.5f;
+		extents.x = abs(extents.x), extents.y = abs(extents.y), extents.z = abs(extents.z);
+		BoundingOrientedBox oriented_box;
+		oriented_box.Center = center;
+		oriented_box.Extents = extents;
+
+		mesh->SetOrientBox(oriented_box);
+
+		return mesh;
+	}
+	void MeshLoader::LoadFrameHierArchy(std::ifstream *rev_file)
+	{
+		std::stringstream ss;
+		std::string line;
+		std::string prefix;
+		
+		std::vector<Mat4> parent;
+		std::vector<Vec3> scale;
+		std::vector<Vec3> rotation;
+		std::vector<Vec3> translation;
+
+		Mat4 temp_mat4;
+		Vec3 temp_vec3;
+		while (ReadFromFile(rev_file,&ss))
+		{
+			ss.clear();
+			ss.str(line);
+			ss >> prefix;
+			switch (HashCode(prefix.c_str()))
+			{
+			case HashCode("<Frame>"):
+				while (std::getline(*rev_file, line))
+				{
+					ss.clear();
+					ss.str(line);
+					ss >> prefix;
+					switch (HashCode(prefix.c_str()))
+					{
+					case HashCode("<Transform>:"):
+						ss >> temp_mat4._11 >> temp_mat4._12 >> temp_mat4._13 >> temp_mat4._14
+							>> temp_mat4._21 >> temp_mat4._22 >> temp_mat4._23 >> temp_mat4._24
+							>> temp_mat4._31 >> temp_mat4._32 >> temp_mat4._33 >> temp_mat4._34
+							>> temp_mat4._41 >> temp_mat4._42 >> temp_mat4._43 >> temp_mat4._44;
+						parent.emplace_back(temp_mat4);
+
+						ss >> temp_vec3.x >> temp_vec3.y >> temp_vec3.z;
+						scale.emplace_back(temp_vec3);
+						ss >> temp_vec3.x >> temp_vec3.y >> temp_vec3.z;
+						rotation.emplace_back(temp_vec3);
+						ss >> temp_vec3.x >> temp_vec3.y >> temp_vec3.z;
+						translation.emplace_back(temp_vec3);
+
+						break;
+						
+					case HashCode("<Mesh>:"):
+						break;
+						
+					case HashCode("<SkinDeformations>:"):
+						break;
+						
+					case HashCode("<Materals>:"):
+						break;
+						
+					case HashCode("<Children>:"):
+						break;
+						
+					case HashCode("</Frame>:"):
+						break;
+
+					}
+				}
+				break;
+			case HashCode("</Hierarchy>"):
+				break;
+			}
+		}
+	}
+	int MeshLoader::ReadFromFile(std::ifstream* file, std::stringstream* word)
+	{
+		word->clear();
+		int length = 0;
+		char buffer[64] = { "\0" };
+		file->read((char *)&length, sizeof(int));
+		file->read(buffer, sizeof(char) * length);
+		word->str(buffer);
+		return length;
 	}
 }
--- Client/ClientFW/src/client/asset/mesh/mesh_loader.h
+++ Client/ClientFW/src/client/asset/mesh/mesh_loader.h
@@ -13,6 +13,10 @@ namespace client_fw
 		virtual SPtr<Mesh> LoadMesh(const std::string& name, const std::string& path, const std::string& extension);
 
 		virtual SPtr<StaticMesh> LoadObj(const std::string& name, const std::string& path, const std::string& extension);
+		virtual SPtr<StaticMesh> LoadRev(const std::string& name, const std::string& path, const std::string& extension);
+		
+		void LoadFrameHierArchy(std::ifstream* rev_file);
+		int ReadFromFile(std::ifstream* file, std::stringstream* word);
 	};
 }
 
