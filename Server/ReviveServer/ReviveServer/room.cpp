9b2ffce531c303eb80a3145ff20ca6ee1a5efe01
undugy <70964651+undugy@users.noreply.github.com>
2022-02-01 20:15:59 +0900

    Room 멤버구성중

--- Server/ReviveServer/ReviveServer/room.cpp
+++ Server/ReviveServer/ReviveServer/room.cpp
@@ -5,6 +5,33 @@ Room::Room()
 {
 }
 
+Room::Room(int room_id):room_id(room_id)
+{
+	curr_user_count = 0;
+}
+
 Room::~Room()
 {
+}
+
+void Room::Init(int user_num, char* name)
+{
+	max_user = user_num;
+	strcpy_s(m_name, name);
+	
+}
+
+bool Room::EnterRoom(int c_id)
+{
+	if (false == CheckUserCount())return false;
+	m_obj_list.insert(c_id);
+	curr_user_count += 1;
+	return true;
+}
+
+void Room::LeaveRoom(int c_id)
+{
+	m_obj_list.erase(c_id);
+	curr_user_count -= 1;
+
 }
--- Server/ReviveServer/ReviveServer/room.h
+++ Server/ReviveServer/ReviveServer/room.h
@@ -1,11 +1,36 @@
 #pragma once
+#include<unordered_set>
+#include<mutex>
+#include<atomic>
 class Room
 {
 public:
-	Room();
+	Room(int room_id) ;
+	Room() = default;
 	~Room();
 
 
+
+	void Init(int user_num, char* name);
+	bool EnterRoom(int c_id);
+	void LeaveRoom(int c_id);
+	void ClearRoom();
+
+	bool CheckUserCount(){ return max_user > curr_user_count; }
+
+	int GetRoomID() { return room_id; }
+	int GetMaxUser() { return room_id; }
+	int GetCurrUserCount() { return curr_user_count; }
+
+
+private:
+	int room_id;
+	int max_user;
+	std::atomic_int curr_user_count;
+	int max_npc;
+	char m_name[MAX_NAME_SIZE];
+	std::mutex m_obj_lock;
+	std::unordered_set<int>m_obj_list;
 
 };
 
